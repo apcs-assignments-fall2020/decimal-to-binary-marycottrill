92182acf58ffd690b870cf12e7867a6b605564c4
marycottrill <21mcottrill@athenian.org>
2020-09-17 03:48:24 +0000

    Binary to Decimal Code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,28 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int secondbinary = Integer.parseInt(binary);
+        double counter = 0;
+        int x = 0;
+        int y = 0;
+        while(secondbinary > 0){
+            x = secondbinary % 2;
+            y += x * Math.pow(2.0, counter);
+            secondbinary = secondbinary / 10;
+            counter++;
+        }
+        System.out.println("This is equal to the decimal value " + y);
+        return y;
+
     }
     
-    
+
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type in a number in binary format:"); 
+        String binary = scan.nextLine(); 
+        binaryToDecimal(binary);
+        scan.close();
     }
 }
+
